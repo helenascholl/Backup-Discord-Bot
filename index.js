ea38d62435e3dd4d6bc89eb342aa5f881496a90f
schollsebastian <scholl.sebastian@gmx.at>
2021-03-02 19:07:29 +0100

    Implement driveApi in index.js

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 const Discord = require('discord.js');
+const got = require('got');
+
+const driveApi = require('./src/driveApi');
+
+const config = require('./config.json');
+
+const imageFileExtensions = [ 'png', 'jpg', 'jpeg' ];
+const mimeTypes = [
+    {
+        mimeType: 'image/jpeg',
+        fileExtensions: [ 'jpg', 'jpeg' ]
+    },
+    {
+        mimeType: 'image/png',
+        fileExtensions: [ 'png' ]
+    }
+];
 
 const client = new Discord.Client();
 
@@ -7,7 +24,64 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    console.log(message.content);
+    if (config.channels.map(c => c.id).includes(message.channel.id)) {
+        const image = getImage(message);
+
+        if (image) {
+            const folderId = config.channels.find(c => c.id === message.channel.id).backupFolderId;
+            const mimeType = mimeTypes
+                .find(mt => mt.fileExtensions.includes(image.filename.split('.').pop()))
+                .mimeType;
+
+            console.log(`Uploading file ${image.filename}`);
+            driveApi.uploadFile(image.filename, folderId, mimeType, got.stream(image.url));
+        }
+    }
 });
 
-client.login(process.env['DISCORD_TOKEN']).catch(console.error);
+initGoogleDrive()
+    .then(startClient);
+
+async function initGoogleDrive() {
+    const rootFolderId = await driveApi.findFolder(config.rootFolder);
+
+    for (const channel of config.channels) {
+        const folderId = await driveApi.findFolder(channel.backupFolder, rootFolderId);
+
+        if (folderId) {
+            console.log(`Folder ${channel.backupFolder} already exists`);
+            channel.backupFolderId = folderId;
+        } else {
+            console.log(`Creating folder ${channel.backupFolder}`);
+            channel.backupFolderId = await driveApi.createFolder(channel.backupFolder, rootFolderId);
+        }
+    }
+}
+
+function startClient() {
+    client.login(process.env['DISCORD_TOKEN']).catch(console.error);
+}
+
+function getImage(message) {
+    for (const attachment of message.attachments) {
+        for (const imageFileExtension of imageFileExtensions) {
+            if (attachment[1].url.endsWith(imageFileExtension)) {
+                return {
+                    filename: attachment[1].name,
+                    url: attachment[1].url
+                }
+            }
+        }
+    }
+
+    for (const embed of message.embeds) {
+        if (embed.type === 'image') {
+            return {
+                filename: embed.url.split('/').pop(),
+                url: embed.url
+            }
+        }
+    }
+
+    return false;
+}
